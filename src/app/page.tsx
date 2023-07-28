70b36579b64291be3f736ea23e7fd6b326bb3453
Muhammad-1254 <usmansoomro1234@gmail.com>
2023-07-28 17:57:35 +0500

    adding navbar animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ export default function Home() {
   return (
     <main className="max-w-[400px] md:max-w-[800px] lg:max-w-[1600px] mx-auto">
         <Navbar/>
+        
+
         <Hero/>
     </main>
   )
