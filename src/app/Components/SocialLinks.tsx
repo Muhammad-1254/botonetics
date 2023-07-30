82b10e17a5ade751d392600e24a684c7ad103c18
Muhammad-1254 <usmansoomro1234@gmail.com>
2023-07-30 19:53:00 +0500

    edit social links

--- src/app/Components/SocialLinks.tsx
+++ src/app/Components/SocialLinks.tsx
@@ -1,7 +1,6 @@
+'use client'
 import React from 'react';
-import { FaGithub, FaLinkedin } from 'react-icons/fa';
-import { HiOutlineMail } from 'react-icons/hi';
-import { BsFillPersonLinesFill } from 'react-icons/bs';
+
 import { socialLinks } from '../constantAPI/api';
 import Link from 'next/link';
 
